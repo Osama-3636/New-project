03eb5750b89a385f81766380767dca6a27a39b3a
Osama-3636 <ooosama.mohd@xed.aucegypt.edu>
2025-05-03 14:02:45 +0200

    test commit

--- newproject/__manifest__.py
+++ newproject/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': "newproject",
-    'version': '17.1',
+    'version': '17.2',
     'summary': "Short (1 phrase/line) summary of the module's purpose",
 
     'description': """
